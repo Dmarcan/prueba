46381641a89f1b87d1e60c31c1b0d29264640a37
David Martinez <david.martinez.c@mail.pucv.cl>
2023-05-12 13:00:58 +0000

    viva xhile

--- main.c
+++ main.c
@@ -5,5 +5,13 @@ int main(void) {
     printf("Hello World\n");
     printf("Hello World\n");
     printf("Hello World\n");
+
+
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+
+
+    printf("Hello World\n");
   return 0;
 }
